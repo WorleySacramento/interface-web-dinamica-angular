9afe166dceb93dfb76d44e8fb5b131e70266241a
Worley Sacramento <worleymartinsns@gmail.com>
2026-09-05 14:53:29 -0300

    feat: Exemplos ngFor

--- src/app/exemplos/diretivas/diretivas.component.ts
+++ src/app/exemplos/diretivas/diretivas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Aluno } from '../aluno';
 
 @Component({
   selector: 'app-diretivas',
@@ -8,12 +9,28 @@ import { Component } from '@angular/core';
 export class DiretivasComponent {
   estaLigado: boolean = false;
   counter: number = 0;
+  listaDeCompras: string[] = ['Arroz', 'Feijão', 'Macarrão',
+    'Carne', 'Frango', 'Ovos', 'Leite', 'Pão', 'Queijo', 'Manteiga'];
 
-  toggle(){
+  toggle() {
     this.estaLigado = !this.estaLigado;
   }
 
-  incrementar(){
+  incrementar() {
     this.counter++;
   }
+
+  alunos: Aluno[] =[
+    { nome: 'João', email: 'joao@email.com', media: 8.5 },
+    { nome: 'Maria', email: 'maria@email.com', media: 9.0 },
+    { nome: 'Pedro', email: 'pedro@email.com', media: 7.5 },
+    { nome: 'Ana', email: 'ana@email.com', media: 8.0 },
+    { nome: 'Carlos', email: 'carlos@email.com', media: 8.5 }
+  ]
+
+  alertar(aluno: Aluno) {
+    alert(`Aluno: ${aluno.nome}\nEmail: ${aluno.email}\nMédia: ${aluno.media}`);
+  }
+
 }
+
