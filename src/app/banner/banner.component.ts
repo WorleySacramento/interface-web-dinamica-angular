6f17de917228328149f7119872841187952012d1
Worley Sacramento <worleymartinsns@gmail.com>
2026-09-05 17:04:40 -0300

    feat: ciclo de vida

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit, OnDestroy {
 
+  hoje: Date = new Date();
+  timerId: any;
+
+  ngOnInit(): void {
+    console.log('BannerComponent foi inicializado.');
+    this.timerId = setInterval(() => {
+      this.hoje = new Date();
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    console.log('BannerComponent foi destruído.');
+    clearInterval(this.timerId);
+  }
+  
 }
